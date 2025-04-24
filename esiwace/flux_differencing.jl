92f6ff76d08cc75bcad7631bb3c974bf08c8c22d
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2025-04-24 15:18:01 +0200

    Initialize du to 0.

--- esiwace/flux_differencing.jl
+++ esiwace/flux_differencing.jl
@@ -66,6 +66,8 @@ integrator = init(ode, CarpenterKennedy2N54(williamson_condition=false),
 
 u = integrator.u
 du_ref = similar(u)
+du_ref .= 0
+du_new .= 0
 du_new = similar(u)
 mesh, equations, solver, cache = Trixi.mesh_equations_solver_cache(ode.p)
 Trixi.calc_volume_integral!(du_ref, u, mesh, Trixi.False(), equations, solver.volume_integral, solver, cache)
