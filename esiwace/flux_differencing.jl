a1be8687d47f6202f3c1ef1ec789a9245994857a
Alessio Sclocco <a.sclocco@esciencecenter.nl>
2025-12-08 14:17:20 +0100

    Experimental split version of the nosym variant.

--- esiwace/flux_differencing.jl
+++ esiwace/flux_differencing.jl
@@ -141,6 +141,17 @@ else
       error_statistics(du_ref, du_exp)
 end
 
+du_exp = similar(u)
+du_exp .= 0
+Trixi.exp_ijk_nosym_split_calc_volume_integral!(du_exp, u, mesh, Trixi.False(), equations, solver.volume_integral, solver, cache)
+
+if all(du_ref .≈ du_exp)
+      println("The exp_ijk_nosym_split version is corrrect.")
+else
+      println("[ERR] There is a BUG in the exp_ijk_nosym_split version.")
+      error_statistics(du_ref, du_exp)
+end
+
 # Tuning
 println()
 best_time = Inf
@@ -268,6 +279,38 @@ while index_x * 32 <= 1024
             try
                   res = @btimed begin
                         Trixi.exp_ijk_nosym_calc_volume_integral!(du_exp, u, mesh, Trixi.False(), equations, solver.volume_integral, solver, cache, wgs)
+                        CUDA.device_synchronize()
+                  end
+                  if res.time < best_time
+                        global best_time = res.time
+                        global best_wgs = wgs
+                  end
+                  global index_y += 1
+            catch
+                  global index_y += 1
+            end
+      end
+      global index_x += 1
+      global index_y = 1
+end
+println("\tBest time: ", best_time, " s -- workgroupsize: ", best_wgs)
+println("Tuning exp_ijk_nosym_split")
+best_time = Inf
+wgs = (0, 0)
+best_wgs = wgs
+index_x = 1
+index_y = 1
+while index_x * 32 <= 1024
+      while index_y <= 32
+            if index_x * index_y > 1024
+                  global index_x += 1
+                  global index_y = 1
+                  continue
+            end
+            global wgs = (index_x * 32, index_y)
+            try
+                  res = @btimed begin
+                        Trixi.exp_ijk_nosym_split_calc_volume_integral!(du_exp, u, mesh, Trixi.False(), equations, solver.volume_integral, solver, cache, wgs)
                         CUDA.device_synchronize()
                   end
                   if res.time < best_time
--- src/solvers/dgsem_p4est/dg_3d_gpu.jl
+++ src/solvers/dgsem_p4est/dg_3d_gpu.jl
@@ -577,6 +577,148 @@ end
     end
 end
 
+@inline function _exp_ijk_nosym_split_calc_volume_integral!(backend::Backend, du, u,
+                                                mesh::P4estMesh{3},
+                                                nonconservative_terms::False, equations,
+                                                volume_integral::VolumeIntegralFluxDifferencing,
+                                                dg::DGSEM, cache, default_wgs)
+    @unpack derivative_split = dg.basis
+    @unpack contravariant_vectors = cache.elements
+    nodes = eachnode(dg)
+    num_nodes = length(nodes)
+    kernel_x! = _exp_ijk_nosym_x_flux_differencing_kernel!(backend)
+    kernel_y! = _exp_ijk_nosym_y_flux_differencing_kernel!(backend)
+    kernel_z! = _exp_ijk_nosym_z_flux_differencing_kernel!(backend)
+
+    kernel_x!(du, u, equations, volume_integral.volume_flux, num_nodes, derivative_split,
+            contravariant_vectors,
+            ndrange = (nelements(dg, cache), num_nodes * num_nodes * num_nodes),
+            workgroupsize = default_wgs)
+    kernel_y!(du, u, equations, volume_integral.volume_flux, num_nodes, derivative_split,
+            contravariant_vectors,
+            ndrange = (nelements(dg, cache), num_nodes * num_nodes * num_nodes),
+            workgroupsize = default_wgs)
+    kernel_z!(du, u, equations, volume_integral.volume_flux, num_nodes, derivative_split,
+            contravariant_vectors,
+            ndrange = (nelements(dg, cache), num_nodes * num_nodes * num_nodes),
+            workgroupsize = default_wgs)
+    return nothing
+end
+
+@kernel function _exp_ijk_nosym_x_flux_differencing_kernel!(du, u, equations,
+                                                volume_flux, num_nodes, derivative_split,
+                                                contravariant_vectors, alpha = true)
+    # true * [some floating point value] == [exactly the same floating point value]
+    # This can (hopefully) be optimized away due to constant propagation.
+    element = ((@index(Group, NTuple)[1] - 1) * @groupsize()[1]) + @index(Local, NTuple)[1]
+    linear_grid = (((@index(Group, NTuple)[2] - 1) * @groupsize()[2]) + @index(Local, NTuple)[2]) - 1
+    i = floor(Int, linear_grid / num_nodes^2) + 1
+    j = floor(Int, (linear_grid % num_nodes^2) / num_nodes) + 1
+    k = (linear_grid % num_nodes) + 1
+    NVARS = Val(nvariables(equations))
+
+    # Calculate volume integral in one element
+    u_node = get_svector(u, NVARS, i, j, k, element)
+
+    # pull the contravariant vectors in each coordinate direction
+    Ja1_node = get_contravariant_vector(1, contravariant_vectors, i, j, k, element)
+
+    # In the original implementation the symmetry of the `volume_flux` is used to save half
+    # of the possible two-point flux computations, and the fact that the diagonal entries
+    # of `derivative_split` are zero.
+    # Here, instead, we naively loop over all indices to avoid conflicting memory access
+
+    # x direction
+    for ii in 1:num_nodes
+        u_node_ii = get_svector(u, NVARS, ii, j, k, element)
+        # pull the contravariant vectors and compute the average
+        Ja1_node_ii = get_contravariant_vector(1, contravariant_vectors,
+                                                ii, j, k, element)
+        Ja1_avg = 0.5 * (Ja1_node + Ja1_node_ii)
+        # compute the contravariant sharp flux in the direction of the
+        # averaged contravariant vector
+        fluxtilde1 = volume_flux(u_node, u_node_ii, Ja1_avg, equations)
+        multiply_add_to_first_axis!(du, alpha * derivative_split[i, ii], fluxtilde1,
+                                    i, j, k, element)
+    end
+end
+
+@kernel function _exp_ijk_nosym_y_flux_differencing_kernel!(du, u, equations,
+                                                volume_flux, num_nodes, derivative_split,
+                                                contravariant_vectors, alpha = true)
+    # true * [some floating point value] == [exactly the same floating point value]
+    # This can (hopefully) be optimized away due to constant propagation.
+    element = ((@index(Group, NTuple)[1] - 1) * @groupsize()[1]) + @index(Local, NTuple)[1]
+    linear_grid = (((@index(Group, NTuple)[2] - 1) * @groupsize()[2]) + @index(Local, NTuple)[2]) - 1
+    i = floor(Int, linear_grid / num_nodes^2) + 1
+    j = floor(Int, (linear_grid % num_nodes^2) / num_nodes) + 1
+    k = (linear_grid % num_nodes) + 1
+    NVARS = Val(nvariables(equations))
+
+    # Calculate volume integral in one element
+    u_node = get_svector(u, NVARS, i, j, k, element)
+
+    # pull the contravariant vectors in each coordinate direction
+    Ja2_node = get_contravariant_vector(2, contravariant_vectors, i, j, k, element)
+
+    # In the original implementation the symmetry of the `volume_flux` is used to save half
+    # of the possible two-point flux computations, and the fact that the diagonal entries
+    # of `derivative_split` are zero.
+    # Here, instead, we naively loop over all indices to avoid conflicting memory access
+
+    # y direction
+    for jj in 1:num_nodes
+        u_node_jj = get_svector(u, NVARS, i, jj, k, element)
+        # pull the contravariant vectors and compute the average
+        Ja2_node_jj = get_contravariant_vector(2, contravariant_vectors,
+                                                i, jj, k, element)
+        Ja2_avg = 0.5 * (Ja2_node + Ja2_node_jj)
+        # compute the contravariant sharp flux in the direction of the
+        # averaged contravariant vector
+        fluxtilde2 = volume_flux(u_node, u_node_jj, Ja2_avg, equations)
+        multiply_add_to_first_axis!(du, alpha * derivative_split[j, jj], fluxtilde2,
+                                    i, j, k, element)
+    end
+end
+
+@kernel function _exp_ijk_nosym_z_flux_differencing_kernel!(du, u, equations,
+                                                volume_flux, num_nodes, derivative_split,
+                                                contravariant_vectors, alpha = true)
+    # true * [some floating point value] == [exactly the same floating point value]
+    # This can (hopefully) be optimized away due to constant propagation.
+    element = ((@index(Group, NTuple)[1] - 1) * @groupsize()[1]) + @index(Local, NTuple)[1]
+    linear_grid = (((@index(Group, NTuple)[2] - 1) * @groupsize()[2]) + @index(Local, NTuple)[2]) - 1
+    i = floor(Int, linear_grid / num_nodes^2) + 1
+    j = floor(Int, (linear_grid % num_nodes^2) / num_nodes) + 1
+    k = (linear_grid % num_nodes) + 1
+    NVARS = Val(nvariables(equations))
+
+    # Calculate volume integral in one element
+    u_node = get_svector(u, NVARS, i, j, k, element)
+
+    # pull the contravariant vectors in each coordinate direction
+    Ja3_node = get_contravariant_vector(3, contravariant_vectors, i, j, k, element)
+
+    # In the original implementation the symmetry of the `volume_flux` is used to save half
+    # of the possible two-point flux computations, and the fact that the diagonal entries
+    # of `derivative_split` are zero.
+    # Here, instead, we naively loop over all indices to avoid conflicting memory access
+
+    # z direction
+    for kk in 1:num_nodes
+        u_node_kk = get_svector(u, NVARS, i, j, kk, element)
+        # pull the contravariant vectors and compute the average
+        Ja3_node_kk = get_contravariant_vector(3, contravariant_vectors,
+                                                i, j, kk, element)
+        Ja3_avg = 0.5 * (Ja3_node + Ja3_node_kk)
+        # compute the contravariant sharp flux in the direction of the
+        # averaged contravariant vector
+        fluxtilde3 = volume_flux(u_node, u_node_kk, Ja3_avg, equations)
+        multiply_add_to_first_axis!(du, alpha * derivative_split[k, kk], fluxtilde3,
+                                    i, j, k, element)
+    end
+end
+
 # /Experiments
 
 @inline function _prolong2interfaces!(backend::Backend, cache, u,
